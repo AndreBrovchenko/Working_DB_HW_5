4fc795b7e3c52495c34ca168d9789b9dac9bef56
Andrey Brovchenko <andrebrjob@mail.ru>
2021-11-27 14:10:21 +0300

    Main Task

---
+++ CREATE.sql
@@ -0,0 +1,59 @@
+﻿-- # входим в режим управления от пользователя postgres (БД тоже postgres)
+psql -U postgres	
+create database music_site_big; # создаем БД с именем music_site_big
+-- # создаем пользователя с именем <user_music> и паролем <pass_music>
+create user user_music with password 'pass_music';
+-- # указываем, что владельцем БД <music_site_big>
+-- # является пользователь <user_music>
+alter database music_site_big owner to user_music;
+-- # -----------------
+-- # входим в режим управления от пользователя user_music (БД music_site_big)
+psql -U user_music music_site_big
+-- # -----------------
+CREATE TABLE Genres (
+id_genre serial primary key,
+genre_name varchar(50) not null
+);
+-- # -----------------
+CREATE TABLE Singers (
+id_singer serial primary key,
+singer_name varchar(50) not null
+);
+-- # -----------------
+CREATE TABLE Albums (
+id_album serial primary key,
+album_name varchar(50) not null,
+year_issue_album integer
+);
+-- # -----------------
+CREATE TABLE AlbumArtists (
+id_album_artist serial primary key,
+id_album integer references Albums (id_album),
+id_singer integer references Singers (id_singer)
+);
+-- # -----------------
+CREATE TABLE PerformerGenres (
+id_performer_genre serial primary key,
+id_singer integer references Singers (id_singer),
+id_genre integer references Genres (id_genre)
+);
+-- # -----------------
+CREATE TABLE AlbumCollections (
+id_album_collection serial primary key,
+album_collection_name varchar(50) not null,
+year_issue_collection integer
+);
+-- # -----------------
+CREATE TABLE tracks (
+id_track serial primary key,
+track_name varchar(50) not null,
+track_duration numeric(3, 2),
+id_album integer references Albums (id_album)
+);
+-- # -----------------
+CREATE TABLE TrackCollections (
+id_track_collection serial primary key,
+id_album_collection integer references AlbumCollections (id_album_collection),
+id_track integer references tracks (id_track)
+);
+-- # -----------------
---
+++ SELECT.sql
@@ -0,0 +1,110 @@
+﻿-- ------------------------------------------------------
+-- 1.количество исполнителей в каждом жанре
+-- ------------------------------------------------------
+SELECT g.genre_name, COUNT(s.singer_name) count_singer
+FROM genres g
+JOIN performergenres p ON g.id_genre = p.id_genre 
+JOIN singers s ON p.id_singer = s.id_singer 
+GROUP BY g.genre_name
+ORDER BY g.genre_name;
+-- ------------------------------------------------------
+-- 2.количество треков, вошедших в альбомы 2019-2020 годов
+-- ------------------------------------------------------
+SELECT a.album_name, a.year_issue_album, COUNT(t.track_name) count_track
+FROM tracks t 
+JOIN albums a ON t.id_album = a.id_album
+WHERE a.year_issue_album between 2019 and 2020
+GROUP BY a.album_name, a.year_issue_album
+ORDER BY a.album_name;
+-- ------------------------------------------------------
+-- 3.средняя продолжительность треков по каждому альбому
+-- ------------------------------------------------------
+SELECT a.album_name, AVG(t.track_duration) avg_track_duration 
+FROM albums a
+JOIN tracks t ON a.id_album = t.id_album 
+GROUP BY a.album_name
+ORDER BY a.album_name;
+-- ------------------------------------------------------
+-- 4.все исполнители, которые не выпустили альбомы в 2020 году
+-- ------------------------------------------------------
+SELECT s.singer_name
+FROM singers s 
+JOIN albumartists aa ON s.id_singer = aa.id_singer 
+JOIN albums a ON aa.id_album = a.id_album 
+WHERE a.year_issue_album != 2020
+GROUP BY s.singer_name
+ORDER BY s.singer_name;
+-- ------------------------------------------------------
+-- 5.названия сборников, в которых присутствует конкретный исполнитель (выберите сами)
+-- ------------------------------------------------------
+SELECT ac.album_collection_name, s.singer_name FROM albumcollections ac
+JOIN trackcollections tc ON ac.id_album_collection = tc.id_album_collection 
+JOIN tracks t ON tc.id_track = t.id_track 
+JOIN albums a ON t.id_album = a.id_album 
+JOIN albumartists aa ON a.id_album = aa.id_album 
+JOIN singers s ON aa.id_singer = s.id_singer 
+WHERE LOWER(s.singer_name) = 'михаил круг'
+GROUP BY ac.album_collection_name, s.singer_name;
+-- ------------------------------------------------------
+-- 6.название альбомов, в которых присутствуют исполнители более 1 жанра
+-- ------------------------------------------------------
+SELECT a.album_name, COUNT(g.genre_name) count_genre FROM albums a 
+JOIN albumartists aa ON a.id_album = aa.id_album 
+JOIN singers s ON aa.id_singer = s.id_singer 
+JOIN performergenres p ON s.id_singer = p.id_singer 
+JOIN genres g ON p.id_genre = g.id_genre 
+GROUP BY a.album_name 
+HAVING COUNT(g.genre_name) > 1
+ORDER BY a.album_name;
+-- ------------------------------------------------------
+-- 7.наименование треков, которые не входят в сборники
+-- ------------------------------------------------------
+SELECT t.track_name FROM tracks t 
+LEFT JOIN trackcollections tc ON t.id_track = tc.id_track
+WHERE tc.id_track is NULL;
+-- ------------------------------------------------------
+-- 8.исполнителя(-ей), написавшего самый короткий по продолжительности трек
+-- ------------------------------------------------------
+SELECT s.singer_name, t.track_duration FROM singers s 
+JOIN albumartists aa ON s.id_singer = aa.id_singer 
+JOIN albums a ON aa.id_album = a.id_album 
+JOIN tracks t ON a.id_album = t.id_album
+WHERE t.track_duration = (SELECT MIN(t.track_duration) FROM tracks t);
+-- ------------------------------------------------------
+-- 9.название альбомов, содержащих наименьшее количество треков
+--   вариант 1 -> Применение предиката ALL в секции HAVING
+-- ------------------------------------------------------
+SELECT ac.album_collection_name, COUNT(t.track_name) 
+FROM albumcollections ac 
+JOIN trackcollections tc ON ac.id_album_collection = tc.id_album_collection 
+JOIN tracks t ON t.id_track = tc.id_track
+GROUP BY ac.album_collection_name
+HAVING COUNT(t.track_name) <= ALL
+	(SELECT COUNT(t.track_name) 
+	FROM albumcollections ac 
+	JOIN trackcollections tc ON ac.id_album_collection = tc.id_album_collection 
+	JOIN tracks t ON t.id_track = tc.id_track
+	GROUP BY ac.album_collection_name)
+;
+-- ------------------------------------------------------
+-- 9.название альбомов, содержащих наименьшее количество треков
+--   вариант 2 -> Применение соединения (JOIN) с условием
+-- ------------------------------------------------------
+SELECT album_collection_name, count_track
+FROM (
+	SELECT ac.album_collection_name, COUNT(t.track_name) count_track
+	FROM albumcollections ac 
+	JOIN trackcollections tc ON ac.id_album_collection = tc.id_album_collection 
+	JOIN tracks t ON t.id_track = tc.id_track
+	GROUP BY ac.album_collection_name) A
+JOIN (
+	SELECT MIN(count_track) min_track
+	FROM (
+		SELECT ac.album_collection_name, COUNT(t.track_name) count_track
+		FROM albumcollections ac 
+		JOIN trackcollections tc ON ac.id_album_collection = tc.id_album_collection 
+		JOIN tracks t ON t.id_track = tc.id_track
+		GROUP BY ac.album_collection_name) A
+	) B on count_track = min_track
+;
+-- ------------------------------------------------------
